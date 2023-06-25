a98557d9773c8521228255b5a3f8a44f6faaa0d5
Magdalena Sarama <owadomorek@gmail.com>
2023-06-25 11:54:49 +0200

    dodawanie produktów do koszyka

--- 16 projekt sklepu/script.js
+++ 16 projekt sklepu/script.js
@@ -1,5 +1,24 @@
 class Basket {
   items = [];
+
+  add(item) {
+    this.items.push(item);
+  }
+}
+
+class Product {
+  constructor(productName, productPrice) {
+    this.name = productName;
+    this.price = productPrice;
+  }
 }
 
 const shopBasket = new Basket();
+
+const oranges = new Product(`Pomarańcze LUZ`, 7.55);
+const cucumbers = new Product(`Ogórek duży`, 8.2);
+
+shopBasket.add(cucumbers);
+shopBasket.add(cucumbers);
+
+console.log(shopBasket);

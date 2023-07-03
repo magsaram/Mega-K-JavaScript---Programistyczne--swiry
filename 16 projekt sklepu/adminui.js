0a62bb22849835538202335b175429c9712f505c
Magdalena Sarama <owadomorek@gmail.com>
2023-07-03 12:17:34 +0200

    dodawanie produktu do listy

---
+++ 16 projekt sklepu/adminui.js
@@ -0,0 +1,36 @@
+const addProductForm = document.querySelector(".form-add-product");
+const nameInput = document.querySelector(".product-name");
+const priceInput = document.querySelector(".product-price");
+const productsUl = document.querySelector(".products-list");
+
+const addProductToShop = (event) => {
+  event.preventDefault(); //nie odświeżaj przeglądarki
+
+  //   console.log(event.target.elements[`product-name`].value); //musimy dodać value, jeśli mamy dostać się do wartości
+
+  const name = nameInput.value;
+  const price = Number(priceInput.value);
+
+  const newLi = document.createElement("li"); //<li>
+
+  const newP = document.createElement("p");
+  newP.innerText = name;
+
+  const newH3 = document.createElement("h3");
+  newH3.innerText = `- ${price}`;
+
+  newBtn = document.createElement("button");
+  newBtn.innerText = "Kup!";
+  newBtn.classList.add("btn-buy-product");
+  newBtn.dataset.name = name;
+  newBtn.dataset.price = price;
+  newBtn.addEventListener("click", addProductToBasket);
+
+  newLi.appendChild(newP);
+  newLi.appendChild(newH3);
+  newLi.appendChild(newBtn);
+
+  productsUl.appendChild(newLi);
+};
+
+addProductForm.addEventListener(`submit`, addProductToShop);

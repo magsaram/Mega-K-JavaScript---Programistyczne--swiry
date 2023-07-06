66875f264b4a8a08c5cb456ac7e84cd1128cc56f
Magdalena Sarama <owadomorek@gmail.com>
2023-07-06 18:44:03 +0200

    Zapisanie listy produktów do Local Storage

--- 16 projekt sklepu/adminui.js
+++ 16 projekt sklepu/adminui.js
@@ -3,14 +3,13 @@ const nameInput = document.querySelector(".product-name");
 const priceInput = document.querySelector(".product-price");
 const productsUl = document.querySelector(".products-list");
 
-const addProductToShop = (event) => {
-  event.preventDefault(); //nie odświeżaj przeglądarki
+const saveProductToLocalStorage = (name, price) => {
+  const productsList = JSON.parse(localStorage.getItem(`shop-product`)) || [];
+  productsList.push({ name, price }); //tworzymy obiekt
+  localStorage.setItem(`shop-product`, JSON.stringify(productsList));
+};
 
-  //   console.log(event.target.elements[`product-name`].value); //musimy dodać value, jeśli mamy dostać się do wartości
-
-  const name = nameInput.value;
-  const price = Number(priceInput.value);
-
+const addProductToShop = (name, price) => {
   const newLi = document.createElement("li"); //<li>
 
   const newP = document.createElement("p");
@@ -33,4 +32,56 @@ const addProductToShop = (event) => {
   productsUl.appendChild(newLi);
 };
 
-addProductForm.addEventListener(`submit`, addProductToShop);
+const loadProductsFromLocalStorage = () => {
+  const productsList = JSON.parse(localStorage.getItem(`shop-product`)) || [];
+
+  for (const product of productsList) {
+    addProductToShop(product.name, product.price);
+  }
+};
+
+const handleAddProductFormSubmit = (event) => {
+  event.preventDefault(); //nie odświeżaj przeglądarki
+
+  //   console.log(event.target.elements[`product-name`].value); //musimy dodać value, jeśli mamy dostać się do wartości
+
+  const nameFromInput = nameInput.value;
+  const priceFromInput = Number(priceInput.value);
+
+  addProductToShop(nameFromInput, priceFromInput);
+  saveProductToLocalStorage(nameFromInput, priceFromInput);
+};
+
+addProductForm.addEventListener(`submit`, handleAddProductFormSubmit);
+
+loadProductsFromLocalStorage();
+
+// const addProductToShop = (event) => {
+//   event.preventDefault(); //nie odświeżaj przeglądarki
+
+//   //   console.log(event.target.elements[`product-name`].value); //musimy dodać value, jeśli mamy dostać się do wartości
+
+//   const name = nameInput.value;
+//   const price = Number(priceInput.value);
+
+//   const newLi = document.createElement("li"); //<li>
+
+//   const newP = document.createElement("p");
+//   newP.innerText = name;
+
+//   const newH3 = document.createElement("h3");
+//   newH3.innerText = `- ${price}`;
+
+//   newBtn = document.createElement("button");
+//   newBtn.innerText = "Kup!";
+//   newBtn.classList.add("btn-buy-product");
+//   newBtn.dataset.name = name;
+//   newBtn.dataset.price = price;
+//   newBtn.addEventListener("click", addProductToBasket);
+
+//   newLi.appendChild(newP);
+//   newLi.appendChild(newH3);
+//   newLi.appendChild(newBtn);
+
+//   productsUl.appendChild(newLi);
+// };

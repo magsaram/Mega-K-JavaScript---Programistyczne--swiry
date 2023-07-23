01c590aa544e4e0f1b750bc8c8756e15cd226670
Magdalena Sarama <owadomorek@gmail.com>
2023-07-23 10:46:42 +0200

    zadanie 2

---
+++ 20 node path/zadanie2.js
@@ -0,0 +1,9 @@
+const { normalize, resolve } = require("path");
+
+function safeJoin(base, target) {
+  const targetPath = `.` + normalize(`/` + target);
+  return resolve(base, targetPath);
+}
+
+const userPath = safeJoin(__dirname, process.argv[2]);
+console.log(userPath);

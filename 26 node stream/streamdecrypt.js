3da7096fa017f5feed572081e8b6ccff67e9854b
Magdalena Sarama <owadomorek@gmail.com>
2023-08-10 18:57:57 +0200

    Zadanie stream

---
+++ 26 node stream/streamdecrypt.js
@@ -0,0 +1,21 @@
+const { pipeline } = require(`stream`).promises;
+const { promisify } = require(`util`);
+const { createDecipher } = require("crypto");
+const { createReadStream, createWriteStream } = require(`fs`);
+const scrypt = promisify(require(`crypto`).scrypt);
+
+(async () => {
+  const inputFile = process.argv[2];
+  const outputFile = process.argv[3];
+  const userPass = process.argv[4];
+  const salt = `98&(^H">MJWOhwofhyfne&*&$YN poU3038{}"E(@*%^&>sjv narrjdsvmOJLKN(&$)"<>!>,.234,))`;
+  const algorithm = `aes-192-cbc`;
+  const key = await scrypt(userPass, salt, 24);
+
+  await pipeline(
+    createReadStream(inputFile),
+    createDecipher(algorithm, key),
+    createWriteStream(outputFile)
+  );
+  console.log(`Done.`);
+})();
